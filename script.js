235b949edaa4f5adb804b08d4497713eb74914e8
nicky202 <rabesoanicky@gmail.com>
2023-12-15 14:05:03 +0100

    last commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var btn1 = document.querySelector("#btnTab1");
 var btn2 = document.querySelector("#btnTab2");
 var btn3 = document.querySelector("#btnTab3");
+var buttons = document.querySelectorAll(".btn");
 var tab = document.querySelector(".slide");
 var border = document.querySelector(".border");
 var questions = document.querySelectorAll(".question");
@@ -9,17 +10,49 @@ var input = document.querySelector("#inputContact");
 var btnContact = document.querySelector("#contact");
 var error = document.querySelector(".error");
 var iconError = document.querySelector("#iconError");
+var close = document.querySelector("#close");
+var menu = document.querySelector("#menu");
+var menuActive = document.querySelector(".mobile-active");
+var listMenu = document.querySelector(".listMenu");
+var body = document.querySelector("body");
+var socialMediaNav = document.querySelector(".socialMediaNav");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    buttons.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    button.classList.add("active");
+  });
+});
+
+menu.addEventListener("click", function () {
+  close.style.display = "block";
+  menu.style.display = "none";
+  menuActive.style.display = "block";
+  listMenu.style.display = "block";
+  body.style.overflow = "hidden";
+  socialMediaNav.style.display = "flex";
+});
+close.addEventListener("click", function () {
+  menu.style.display = "block";
+  close.style.display = "none";
+  menuActive.style.display = "none";
+  listMenu.style.display = "none";
+  body.style.overflow = "auto";
+  socialMediaNav.style.display = "none";
+});
 
 btn1.addEventListener("click", () => {
-  tab.style.left = "-50vw";
+  tab.style.left = "0vw";
   border.style.left = 0;
 });
 btn2.addEventListener("click", () => {
-  tab.style.left = "-150vw";
+  tab.style.left = "-100vw";
   border.style.left = "11rem";
 });
 btn3.addEventListener("click", () => {
-  tab.style.left = "-250vw";
+  tab.style.left = "-200vw";
   border.style.left = "22rem";
 });
 
@@ -53,3 +86,7 @@ btnContact.addEventListener("click", () => {
     input.style.border = "1px solid hsl(229, 31%, 21%)";
   }
 });
+
+// close.addEventListener('click',function(){
+
+// })
